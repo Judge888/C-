5f5dda14f329537465c6ce916bd92bc2dec70fcf
Judge <1462049744@qq.com>
2022-07-19 23:11:03 +0800

    night

---
+++ test_2022_7_19/循环语句.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+	int a = 0;
+	while (a < 30)
+	{
+		printf("l love you ", a);
+		a++;
+	}
+	if (a = 30)
+	{
+		printf("believe me");
+	}
+
+	return 0;
+}
---
+++ test_2022_7_19/选择语句.c
@@ -0,0 +1,15 @@
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int coding = 0;
+//	printf("do you want to join my team?\n1 or 0\n");
+//	scanf_s("%d", &coding);
+//	if (coding == 1)
+//		printf("welcome to my team");
+//	else
+//		printf("l respect your choice");
+//
+//
+//	return 0;
+//}
